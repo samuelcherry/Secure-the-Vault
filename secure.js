48ac4f674b97ecb6bc7e7c009d00b5e28470c5a8
Sam Cherry <76022735+samuelcherry@users.noreply.github.com>
2024-11-04 10:26:38 -0700

    created the code combination message

---
+++ secure.js
@@ -0,0 +1,15 @@
+// Create 3 variables for the 3 parts of the code
+// for each var have a math equation that gives the correct number
+// create an alert that will use string literals to show the correct code
+
+//the main message
+let message =
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination: ";
+
+//Declaring the variables
+const code1 = 8 + 2;
+const code2 = code1 * 4;
+const code3 = code2 - 1;
+
+//creating the alerts
+alert(`${message}${code1}-${code2}-${code3}`);
